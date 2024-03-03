dead3c8833e84d6f8f4cd635caf2c67ef108ba71
Gabriel S. Guerra <159430864+gabriel-guerra@users.noreply.github.com>
2024-03-03 16:55:05 +0000

    v1.0

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+import fs from "fs";
 
 const result = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
 const resultBody = await result.json();
-console.log(resultBody);
 
-var pokemons;
+var pokemons = [];
 
 for (var ps of resultBody.results){
     
@@ -12,13 +12,17 @@ for (var ps of resultBody.results){
 
     let pokemon = {
         nome: pokemonInfo.name,
-        //tipo: [pokemonInfo.type.name],
+        tipo: pokemonInfo.types.map(type => type.type.name),
         peso: pokemonInfo.weight,
         altura: pokemonInfo.height,
-        pokedex: pokemonInfo.id,
-        //sprite: pokemonInfo.sprites.front_default
+        pokedex: pokemonInfo.game_indices.find(index => index.version.name === 'red')?.game_index || null,
+        sprite: pokemonInfo.sprites.front_default
     }
 
-    console.log(pokemon);
+    pokemons.push(pokemon);
 
 }
+
+fs.writeFile("pokemon.json", JSON.stringify(pokemons, null, 2), (error) => {
+    console.log(error);
+});
